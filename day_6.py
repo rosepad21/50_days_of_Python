f1244f994f948ba192290b40440f7274a7df9800
midorukawa <115275132+midorukawa@users.noreply.github.com>
2022-12-01 12:24:52 +0100

    Update day_6.py

--- day_6.py
+++ day_6.py
@@ -1,12 +1,16 @@
+# Day 6: User Name Generator
 def user_name():
-    inp = input('Enter an email: ')
-    return inp[:inp.find('@')]
+    inp = input('Enter an email: ') # we ask the user for a mail
+    if inp.find('@') == -1:
+        return 'Not a valid email'
+    else:
+        return inp[:inp.find('@')] # we return the username
 
-print(user_name())
-
+# Extra Challenge: Zero both ends
 def zeroed(li):
+    if len(li) == 0:
+        return -1
+    # given a list of numbers, we change the first and the last numbers to zero
     li[0] = 0
     li[-1] = 0
     return li
-
-print(zeroed([2,3,4,5,1]))
