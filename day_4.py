510f2e0504581bb496dc05c43e4ab4d7325e5a17
midorukawa <115275132+midorukawa@users.noreply.github.com>
2022-12-01 12:16:53 +0100

    Update day_4.py

--- day_4.py
+++ day_4.py
@@ -1,22 +1,18 @@
+# Day 4: Only Floats
 def only_floats(a, b):
     if isinstance(a, float) and isinstance(b, float):
-        return 2
+        return 2 # returning 2 if both arguments are floats
     elif isinstance(a, float) or isinstance(b, float):
-        return 1
+        return 1 # returning 1 if only one argument is a float
     else:
-        return 0
+        return 0 # returning 0 if none are floats
 
-
-print(only_floats(12.1, 23.3))
-
-
+# Extra challenge: Index of the Longest Word
 def word_index(words):
     length = []
     for i in words:
-        length.append(len(i))
+        length.append(len(i)) # appending lengths of all the words in the list
     if length.count(max(length)) > 1:
         return 0
     else:
-        return length.index(max(length))
-
-print(word_index(['Hate', 'love']))
+        return length.index(max(length)) # returning the index of the longest word
