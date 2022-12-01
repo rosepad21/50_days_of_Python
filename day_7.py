df35bd301dcb0fb9000c34aa2729be55d4dce926
midorukawa <115275132+midorukawa@users.noreply.github.com>
2022-12-01 12:27:39 +0100

    Update day_7.py

--- day_7.py
+++ day_7.py
@@ -1,18 +1,17 @@
-def string_range(n):
+# Day 7: A string range
+def string_range(n): # given a number, we return a string of its range separated by dots
     strs = ''
     for i in range(n):
         strs += str(i)
         strs += '.'
-    return strs.rstrip('.')
+    return strs.rstrip('.') # since a last dot would be added after the last number, we remove it
 
-print(string_range(6))
-
+# Extra Challenge: Dictionary of Names
 def dictionary_names(li):
     di = dict()
+    if len(li) == 0:
+        return 'The list is empty'
     for i in li:
-        if i.startswith('S'):
+        if i.startswith('S'): # we have to return all the names that start with S and how many times they appear in the list
             di[i] = di.get(i, 0) + 1
     return di
-
-print(dictionary_names(["Joseph","Nathan", "Sasha", "Kelly",
- "Muhammad", "Jabulani", "Sera", "Patel", "Sera"]))
