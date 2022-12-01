249d8a1f1aa23913eb0e3facae0d64a6bca65057
midorukawa <115275132+midorukawa@users.noreply.github.com>
2022-12-01 12:06:53 +0100

    Update day_1.py

--- day_1.py
+++ day_1.py
@@ -1,17 +1,14 @@
-# Division and Square root
+# Day 1: Division and Square root
 import math
 
 def divide_or_square(n):
-    if n % 5 == 0:
+    if n % 5 == 0: # if the number is divisible by 5 we return its square root
         return round(math.sqrt(n), 2)
-    else:
+    else: # else we return its remainder
         return n % 5
 
 
-#print(divide_or_square(9))
-
+# Extra Challenge: Longest Value
 def longest_value(di):
-    return max(di.values(), key=len)
-
-print(longest_value({'fruit': 'apple', 'color':'green'}))
+    return max(di.values(), key=len) # finding the longest first value in a dictionary
 
