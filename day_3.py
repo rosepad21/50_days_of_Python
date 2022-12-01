7212f7db94f94e0f8c79eee8dd834d2859e39e89
midorukawa <115275132+midorukawa@users.noreply.github.com>
2022-12-01 12:13:48 +0100

    Update day_3.py

--- day_3.py
+++ day_3.py
@@ -1,24 +1,17 @@
+# Day 3: Register Check
 def register_check(di):
     count = 0
     for value in di.values():
-        if value == 'yes':
+        if value == 'yes': # checking students that are in school
             count += 1
-    return count
+    return count # returning the sum of students that are in school
 
-print(register_check({'Michael':'yes','John': 'no', 
- 'Peter':'yes', 'Mary': 'yes'}))
-
-
-
-
+# Extra Challenge: Lowercase Names
 def lower_names(names):
     lower = []
     for i in names:
         if i.islower():
-            lower.append(i)
+            lower.append(i) # creating a list of all names in the list that are lowercase
         else:
             continue
-    return tuple(lower)
-
-names = ["kerry", "dickson", "John", "Mary", "carol", "Rose", "adam"]
-print(lower_names(names))
+    return tuple(lower) # returning it as a tuple
