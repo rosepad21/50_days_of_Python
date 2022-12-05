7d170c3ff455d68ff9fd7048bc05262dab6acaa5
midorukawa <115275132+midorukawa@users.noreply.github.com>
2022-12-05 17:48:39 +0100

    Create day_12.py

---
+++ day_12.py
@@ -0,0 +1,18 @@
+# Day 12: Count the Dots
+def count_dots(strs):
+    return list(strs).count('.')
+
+print(count_dots('h.e.l.p'))
+
+# Extra Challenge: Your age in minutes
+def age_in_minutes():
+    while True:
+        inp = input('Enter your year of birth: ')
+        if len(inp) > 4:
+            print('Input invalid. Please input a valid year of birth.')
+            continue
+        else:
+            break
+    
+		leap_years = (2022-int(inp)) // 4
+    return 'You are ' + str(((2022-int(inp)-leap_years) * (60*24*365) + (leap_years * 60 * 24 * 366))) + ' minutes old'
